81e01509e50f827828fb2338367a433a32d5cdc4
Nikita Zhamoidzik <gorobot.nz@gmail.com>
2023-05-16 17:23:57 +0300

    Add state change

--- internal/actions/srvactions.go
+++ internal/actions/srvactions.go
@@ -2,15 +2,29 @@ package actions
 
 import (
 	ipb "github.com/slntopp/nocloud-proto/instances"
+	stpb "github.com/slntopp/nocloud-proto/states"
+	"github.com/slntopp/nocloud/pkg/states"
 	"google.golang.org/protobuf/types/known/structpb"
 )
 
-type ServiceAction func(*ipb.Instance, map[string]*structpb.Value) (*ipb.InvokeResponse, error)
+type ServiceAction func(states.Pub, *ipb.Instance, map[string]*structpb.Value) (*ipb.InvokeResponse, error)
 
 var SrvActions = map[string]ServiceAction{
-	"change_status": ChangeStatus,
+	"change_status": ChangeState,
 }
 
-func ChangeStatus(inst *ipb.Instance, data map[string]*structpb.Value) (*ipb.InvokeResponse, error) {
-	return nil, nil
+func ChangeState(pub states.Pub, inst *ipb.Instance, data map[string]*structpb.Value) (*ipb.InvokeResponse, error) {
+	state := int32(data["state"].GetNumberValue())
+	statepb := stpb.NoCloudState(state)
+
+	inst.State.State = statepb
+
+	go pub(&stpb.ObjectState{
+		Uuid:  inst.GetUuid(),
+		State: inst.GetState(),
+	})
+
+	return &ipb.InvokeResponse{
+		Result: true,
+	}, nil
 }
--- internal/server/invoke.go
+++ internal/server/invoke.go
@@ -2,10 +2,30 @@ package server
 
 import (
 	"context"
+	"github.com/slntopp/nocloud-driver-virtual/internal/actions"
+	accesspb "github.com/slntopp/nocloud-proto/access"
 	pb "github.com/slntopp/nocloud-proto/drivers/instance/vanilla"
 	ipb "github.com/slntopp/nocloud-proto/instances"
+	"go.uber.org/zap"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (s *VirtualDriver) Invoke(ctx context.Context, req *pb.InvokeRequest) (*ipb.InvokeResponse, error) {
-	return nil, nil
+	s.log.Debug("Invoke request received", zap.Any("action", req.Method))
+
+	method := req.GetMethod()
+	instance := req.GetInstance()
+
+	if instance.GetAccess().GetLevel() < accesspb.Level_ROOT {
+		return nil, status.Errorf(codes.PermissionDenied, "Action %s is admin action", method)
+	}
+
+	actions, ok := actions.SrvActions[method]
+
+	if !ok {
+		return nil, status.Errorf(codes.NotFound, "Action %s not declared for %s", method, s.Type)
+	}
+
+	return actions(s.HandlePublishInstanceState, instance, req.GetParams())
 }
