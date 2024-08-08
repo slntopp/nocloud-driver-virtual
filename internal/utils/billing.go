3ff6fa5a64a9c809e2c88dda5acf0c2ebeec57e1
coddmeistr <euseew.maxim2015@yandex.ru>
2024-08-08 18:48:56 +0300

    fix

--- internal/utils/billing.go
+++ internal/utils/billing.go
@@ -7,8 +7,12 @@ import (
 )
 
 func SendActualMonitoringData(copiedData map[string]*structpb.Value, actualData map[string]*structpb.Value, instance string, publisher i.Pub) {
-	copiedData["actual_last_monitoring"] = actualData["last_monitoring"]
-	copiedData["actual_next_payment_date"] = actualData["next_payment_date"]
+	if val, ok := actualData["last_monitoring"]; ok {
+		copiedData["actual_last_monitoring"] = val
+	}
+	if val, ok := actualData["next_payment_date"]; ok {
+		copiedData["actual_next_payment_date"] = val
+	}
 	_, _ = publisher(&instances.ObjectData{
 		Uuid: instance,
 		Data: copiedData,
